d3bc4adbe843ec85964f482a1aa3b8c14fa16fba
Andreas Prudzilko <mendrik76@gmail.com>
2017-09-15 16:22:26 +0300

    frontend infra tweaks

--- frontend/build/build.ts
+++ frontend/build/build.ts
@@ -5,25 +5,16 @@ import * as zlib from 'zlib'
 console.log(' *** building frontend ***')
 
 const feather = fs.readFileSync('node_modules/feather-ts/feather.min.js', 'utf-8')
-const components = fs.readFileSync('feather-components/feather-components.js', 'utf-8')
+const components = fs.readFileSync('node_modules/feather-components/feather-components.min.js', 'utf-8')
 const quill = fs.readFileSync('out/quill.js', 'utf-8')
 
 const minified = UglifyJS.minify([feather, components, quill])
 
 fs.writeFileSync('out/quill.min.js', minified.code)
 
-const compressor1 = zlib.createGzip({level: 1})
-compressor1.pipe(fs.createWriteStream('out/quill.min.js.gz'))
-compressor1.end(minified.code)
-
-const compressor2 = zlib.createGzip({level: 1})
-compressor2.pipe(fs.createWriteStream('out/styles.css.gz'))
-compressor2.end(fs.readFileSync('out/styles.css', 'utf-8'))
-
 console.log(' *** copy to public ***')
 
-fs.createReadStream('out/quill.min.js.gz').pipe(fs.createWriteStream('../public/quill.min.js.gz'));
-fs.createReadStream('out/styles.css.gz').pipe(fs.createWriteStream('../public/styles.css.gz'));
 fs.createReadStream('out/quill.min.js').pipe(fs.createWriteStream('../public/quill.min.js'));
 fs.createReadStream('out/styles.css').pipe(fs.createWriteStream('../public/styles.css'));
+fs.createReadStream('node_modules/feather-components/feather-components.css').pipe(fs.createWriteStream('../public/feather-components.css'));
 fs.createReadStream('node_modules/bulma/css/bulma.css').pipe(fs.createWriteStream('../public/bulma.css'));
