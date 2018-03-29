caf1c2bb28e96f72a67085b0c257b2763df23a5e
Andreas Prudzilko <mendrik76@gmail.com>
2018-03-29 12:36:33 +0300

    add close handler to modals

--- frontend/typescript/modals/modal-manager.ts
+++ frontend/typescript/modals/modal-manager.ts
@@ -6,6 +6,7 @@ module quill.modal {
     import Subscribe     = feather.hub.Subscribe
     import Widget        = feather.core.Widget
     import Bind          = feather.observe.Bind
+    import On = feather.event.On;
 
     export abstract class ModalWidget extends Widget {
         abstract getTitle()
@@ -35,19 +36,29 @@ module quill.modal {
             this.showing = true
         }
 
+        @On({event: 'mousedown', selector: '.modal-background,button.is-cancel,button.delete'})
+        close() {
+            this.showing = false
+        }
+
+        @On({event: 'click', selector: 'button.is-cancel,button.delete'})
+        closeButtons() {
+            this.close()
+        }
+
         @Template()
         markup() {
             return `<div class="modal {{showing:toActive}}">
               <div class="modal-background"/>
-              <div class="modal-card">
+              <div class="modal-card is-small">
                 <header class="modal-card-head">
                   <p class="modal-card-title">{{title:translate}}</p>
                   <button class="delete" aria-label="close"/>
                 </header>
-                <section class="modal-card-body" {{modal}}/>
+                <section class="modal-card-body is-small" {{modal}}/>
                 <footer class="modal-card-foot">
-                  <button class="button is-success">{{successButton:translate}}</button>
-                  <button class="button">{{cancelButton:translate}}</button>
+                  <button class="button is-inverted is-info">{{successButton:translate}}</button>
+                  <button class="button is-inverted is-info is-cancel">{{cancelButton:translate}}</button>
                 </footer>
               </div>
             </div>`
