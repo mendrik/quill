e0ba6d513b09ab95368bbacc4a61c8c71338d6de
Andreas Prudzilko <mendrik76@gmail.com>
2018-03-30 20:52:30 +0300

    add vertical tabs

--- frontend/typescript/modals/node-config.ts
+++ frontend/typescript/modals/node-config.ts
@@ -21,8 +21,16 @@ module quill.modal {
             return `
             <tabs tabs-class="is-boxed">
                 <div title="ui.modal.node-config.tabs.text"
-                     tooltip="Bla"
-                     icon=${TreeNodeIcon.text} active></div>
+                     icon=${TreeNodeIcon.text} active>
+                    <tabs class="vertical">
+                        <div title="ui.modal.node-config.text.single-line" icon="font" active>
+                            ABC
+                        </div>
+                        <div title="ui.modal.node-config.text.multi-line" icon="align-justify">
+                            EFG
+                        </div>
+                    </tabs>
+                </div>
                 <div title="ui.modal.node-config.tabs.number"
                      icon=${TreeNodeIcon.number}></div>
                 <div title="ui.modal.node-config.tabs.date"
