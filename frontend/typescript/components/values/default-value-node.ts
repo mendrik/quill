ef96b8698d4380a2bbbb58ba33d1a386d75ea266
Andreas Prudzilko <mendrik76@gmail.com>
2018-04-06 17:40:28 +0300

    add value node select

--- frontend/typescript/components/values/default-value-node.ts
+++ frontend/typescript/components/values/default-value-node.ts
@@ -1,8 +1,12 @@
 module quill {
 
     import Template = feather.annotations.Template
+    import Bind = feather.observe.Bind
+    import Subscribe = feather.hub.Subscribe
 
     export class DefaultValueNode extends ValueNode {
+
+        @Bind({}) selected = false
 
         constructor(node: CustomTreeNode, value?: Value) {
             super(node, value)
@@ -11,13 +15,20 @@ module quill {
         @Template()
         markup() {
             return `
-            <li class="value-line default">
+            <li class="value-line default {{selected:selectedClass}}">
                 <span>{{node.value.name}}</span>
                 <span class="icon is-small">
                     <i class="fa {{icon}}"></i>
                 </span>
             </li>`
         }
+
+        @Subscribe('defocus-other-nodes')
+        defocusByNode(node: CustomTreeNode) {
+            this.selected = this.node === node
+        }
+
+        selectedClass = (selected: boolean) => selected ? 'selected' : undefined
     }
 }
 
